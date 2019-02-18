6770423bb57cfb601547ad70fe8174c2d8d48a98
white_zhang <30748865@qq.com>
2019-02-19 02:25:35 +0800

    init antd

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button } from 'antd'
+import './index.less'
+
+export default class Index extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <div className="content">111</div>
+                <Button type='primary'>111</Button>
+            </div>
+        )
+    }
+}
